aa09d619fb3bf503f27e31f88053b67935366182
unknown <kmpujar2014@gmail.com>
2020-11-18 11:49:44 +0500

    Added Listview for published post and drafts post

--- Blog_Site/Blog_App/urls.py
+++ Blog_Site/Blog_App/urls.py
@@ -4,4 +4,6 @@ from Blog_App import views
 urlpatterns=[
     path('',views.PostListView.as_view(),name='post_list'),
     path('about/',views.AboutView.as_view(),name='about'),
+    path('post/<str:pk>',views.PostDetailView.as_view(),name='post_detail'),
+    path('drafts/',views.DraftListView.as_view(),name='post_draft_list'),
 ]
--- Blog_Site/Blog_App/views.py
+++ Blog_Site/Blog_App/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView,ListView
+from django.views.generic import TemplateView,ListView,DetailView
 from .models import Post,Comment
 from django.utils import timezone
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
 class AboutView(TemplateView):
@@ -12,3 +13,15 @@ class PostListView(ListView):
 
     def get_queryset(self):
         return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+
+class PostDetailView(DetailView):
+    model=Post
+
+class DraftListView(LoginRequiredMixin,ListView):
+    login_url='/accounts/login'
+    
+
+    model=Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=True)
